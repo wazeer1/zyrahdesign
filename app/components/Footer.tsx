3fbff91a4474abb15186b0c3907e335334d8ced6
ahammadsalim1234 <ajmals.oraak@gmail.com>
2025-12-05 20:44:43 +0530

    add address

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -12,7 +12,33 @@ export default function Footer() {
       </h3>
 
       <div className="text-sm mb-6 space-y-2">
+        
         {/* Email Contact */}
+        
+
+        {/* Address */}
+        <p className="flex items-start justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="w-5 h-5 mr-3 mt-0.5 shrink-0"
+            style={{ color: "#B89C60" }}
+          >
+            <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0z" />
+            <circle cx="12" cy="10" r="3" />
+          </svg>
+          <span style={{ color: "#F8F8F8" }} className="">
+            Beauty Bloom Cosmetics Trading FZE
+            Business Centre, <br /> Sharjah Publishing City Free Zone,<br /> Sharjah, United Arab Emirates
+          </span>
+        </p>
         <p className="flex items-center justify-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
